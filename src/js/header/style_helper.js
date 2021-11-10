0582335d4e9b3e71b98f70af68cb4e926383367e
Nicolas Alvarez <nicolas.doo@hotmail.com>
2021-11-10 18:31:54 -0500

    On branch issue_#3
     Add coments on style_helper file.
    
     Your branch is up to date with 'origin/issue_#3'.
    
     Changes to be committed:
    	modified:   src/js/header/style_helper.js

--- src/js/header/style_helper.js
+++ src/js/header/style_helper.js
@@ -1,3 +1,11 @@
+/*
+ * scrooll_from_top
+ *
+ * Args:
+ *
+ * This function is the distance that has been scrolled 
+ * from the beginning of the page.
+ */
 function scroll_from_top() {
     var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop
     return scrollTop;
@@ -6,12 +14,14 @@ function scroll_from_top() {
 /*
  * Toggle '.header-scrolled' class to #header when page is scrolled
  */
-window.onscroll = (event) => {
-    var header = document.getElementById("header");
-    var scrollTop = scroll_from_top()
-    if (scrollTop > 100) {
-        header.classList.add('header-scrolled');
-    } else {
-        header.classList.remove('header-scrolled');
+if (document.getElementById("header")) {
+    window.onscroll = (event) => {
+        var header = document.getElementById("header");
+        var scrollTop = scroll_from_top()
+        if (scrollTop > 100) {
+            header.classList.add('header-scrolled');
+        } else {
+            header.classList.remove('header-scrolled');
+        }
     }
 }
