b22f49076acc8bcbdfa8ef00dde545c3641cc687
Nicolas Alvarez <nicolas.doo@hotmail.com>
2022-02-03 15:33:46 -0500

    First version of Modal class is finished.
     On branch admin_portal
     Your branch is ahead of 'origin/admin_portal' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   src/js/admin/modals/Modal.js
    	modified:   src/js/admin/modals/group_modals.js

--- src/js/admin/modals/Modal.js
+++ src/js/admin/modals/Modal.js
@@ -1,11 +1,19 @@
 
 class Modal {
 
-    constructor() {
+    constructor(modal_id, params=params_start) {
+
+        for(let k of Object.keys(params)) {
+            params_start[k] = params[k];
+        }
 
         var modal = document.createElement('div');
-        modal.classList.add('modal');
-        modal.setAttribute('tabindex', '-1')
+        modal.classList.add('modal', 'fade');
+        modal.setAttribute('tabindex', '-1');
+        modal.setAttribute('data-bs-backdrop', 'static');
+        modal.setAttribute('data-bs-keyboard', 'false');
+        modal.setAttribute('aria-hidden', 'true');
+        modal.id = modal_id;
 
         var modal_dialog = document.createElement('div');
         modal_dialog.classList.add('modal-dialog', 'modal-dialog-centered', 'modal-dialog-scrollable', 'modal-lg');
@@ -20,7 +28,7 @@ class Modal {
         modal_content.appendChild(modal_header);
         var h5 = document.createElement('h5');
         h5.classList.add('modal-title');
-        h5.innerHTML = 'Titulo'
+        h5.innerHTML = params_start['title'];
         modal_header.appendChild(h5);
         var xButoon = document.createElement('button');
         xButoon.type = 'button';
@@ -37,21 +45,39 @@ class Modal {
         var modal_footer = document.createElement('div')
         modal_footer.classList.add('modal-footer');
         modal_content.appendChild(modal_footer);
+        var contetn_footer = document.createElement('div');
         var yButoon = document.createElement('button');
         yButoon.type = 'button';
         yButoon.classList.add('btn', 'btn-secondary');
         yButoon.setAttribute('data-bs-dismiss', 'modal');
-        yButoon.innerHTML = 'Cerrar';
+        yButoon.innerHTML = params_start['closeButton_label'];
+        modal_footer.appendChild(contetn_footer)
         modal_footer.appendChild(yButoon);
 
-
-        xButoon.addEventListener('click', ()=> this.hide());
-        yButoon.addEventListener('click', ()=> this.hide());
-
-
         this.modal = $(modal);
-        this.title = $(h5);
         this.body = $(modal_body);
+        this.footer = $(modal_footer);
+        this._title = $(h5);
+        this._close_button = $(yButoon);
+        this._conten_footer = $(contetn_footer);
+
+        if(params_start['body_content'])
+            this.set_bodyContent(params_start['body_content']);
+
+        if(params_start['extraFooter_content'])
+            this.set_footerContent(params_start['extraFooter_content']);
+
+        if(!params_start['closeButton_visible']) 
+            this.hide_closeButton();
+
+        if(!params_start['footer_visible'])
+            this.hide_footer();
+
+        if(params_start['onClick_toggle'])
+            this.add_onClick_toggle(params_start['onClick_toggle']);
+
+        if(params_start['appendTo'])
+            this.appendTo(params_start['appendTo']);
 
     }
 
@@ -61,6 +87,14 @@ class Modal {
 
     get id() {
         return this.modal.attr('id');
+    }
+
+    set title(value) {
+        this._title.text(value);
+    }
+
+    get title() {
+        return this._title.text();
     }
 
     show() {
@@ -78,6 +112,46 @@ class Modal {
     appendTo(node) {
         this.modal.appendTo(node);
     }
+
+    add_onClick_toggle(node) {
+        node.setAttribute('data-bs-toggle', 'modal')
+        node.setAttribute('data-bs-target', `#${this.id}`)
+    }
+
+    set_bodyContent(node) {
+        this.body.html(node);
+    }
+
+    set_footerContent(node) {
+        this._conten_footer.html(node);
+    }
+
+    hide_closeButton() {
+        this._close_button.addClass('d-none');
+    }
+
+    show_closeButton() {
+        this._close_button.removeClass('d-none');
+    }
+
+    hide_footer() {
+        this.footer.addClass('d-none');
+    }
+
+    show_foother() {
+        this.removeClass('d-none');
+    }
+}
+
+const params_start = {
+    title: "Titulo",
+    closeButton_label: "Cerrar",
+    closeButton_visible: true,
+    extraFooter_content: null,
+    body_content: null,
+    footer_visible: true,
+    appendTo: null,
+    onClick_toggle: null
 }
 
 export { Modal };
--- src/js/admin/modals/group_modals.js
+++ src/js/admin/modals/group_modals.js
@@ -1,16 +1,17 @@
 import { Modal } from "./Modal.js";
 
-
 $(document).ready( function () {
     var modal_section = document.getElementById('modal_section');
+    var add_button = document.getElementById('btn-addGroup');
 
-    var agregar = new Modal();
-    agregar.appendTo(modal_section);
-    agregar.id = 'myModal';
-
-    var myModal = new bootstrap.Modal(document.getElementById(agregar.id), {})
-
-    $('#btn-addGroup').click(()=>{
-        agregar.show();
+    var modal_add = new Modal('add_modal', {
+        title: 'Nuevo Grupo',
+        onClick_toggle: add_button,
+        appendTo: modal_section,
+        closeButton_label: 'Cancelar y salir',
+        footer_visible: false
     });
+
+    //modal_add.set_bodyContent(div);
+
 });
