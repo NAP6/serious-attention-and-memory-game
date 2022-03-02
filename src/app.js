8e01e1a286af7eddc071402aa9b0538c2523dedd
Nicolas Alvarez <nicolas.doo@hotmail.com>
2022-03-02 10:13:33 -0500

    Use enviroment variables to conect to MySQL database.
     On branch database
     Your branch is ahead of 'origin/database' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   src/app.js
    	modified:   src/back/database/database.js
    	deleted:    src/back/database/database.sql

--- src/app.js
+++ src/app.js
@@ -5,8 +5,17 @@ import {fileURLToPath} from 'url';
 import { Server } from "./back/Server.js";
 import { Router } from "./back/Routes/routes.js";
 import { api } from "./back/Routes/api.js";
+import { database } from "./back/database/database.js";
+import dotenv from 'dotenv';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+dotenv.config();
+
+var DB_HOST = process.env.DB_HOST || 'localhost';
+var DB_PORT = process.env.DB_PORT || '3306';
+var DB_NAME = process.env.DB_NAME || 'serious_game';
+var DB_USER = process.env.DB_USER || 'root';
+var DB_PASS = process.env.DB_PASS || 'root';
 
 var PORT = 3000;
 var MODELS_PATH = join(__dirname, 'models');
@@ -14,5 +23,6 @@ var FRONT_PATH = join(__dirname, 'front');
 var FRONT_ASSETS_PATH =FRONT_PATH + '/assets';
 var router = Router(FRONT_PATH);
 
+database.set_parameters(DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME);
 var server = new Server(PORT, [MODELS_PATH, FRONT_ASSETS_PATH], [router, api]);
 server.start();
--- src/back/database/database.js
+++ src/back/database/database.js
@@ -2,20 +2,23 @@ import mysql from "mysql2/promise";
 
 class database {
     static host = "localhost";
+    static port = 3306;
     static user = "root";
     static password = "root";
     static database = "serious_game";
 
-    static set_parameters(host, user, password, database) {
-        database.host = host;
-        database.user = user;
-        database.password = password;
-        database.database = database;
+    static set_parameters(host, port, user, password, database) {
+        this.host = host;
+        this.user = user;
+        this.port = port;
+        this.password = password;
+        this.database = database;
     }
 
     static async connect() {
         var connection = await mysql.createConnection({
             host: database.host,
+            port: database.port,
             user: database.user,
             password: database.password,
             database: database.database
